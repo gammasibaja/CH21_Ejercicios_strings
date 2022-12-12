a7230074b09357977192daa78c387673ff719fe3
Gamaliel Sibaja <gamsibaja@gmail.com>
2022-12-12 15:38:44 -0600

    se realizo el ejercicio 2 de strings

---
+++ js/ejercicio2.js
@@ -0,0 +1,21 @@
+// let nombre2= "Gamaliel";
+
+// function reversa (){
+//     let resultado ="";
+//     for (let index = nombre2.length; index >=0; index--) {
+//         resultado += nombre2[index];
+//     }
+//     return resultado;
+// }
+
+// reversa (nombre2);
+
+function reves(str) {
+    let tmpStr ="";
+    for (let indice = str.length-1 ; indice >= 0; indice--) {
+        tmpStr += str.charAt(indice)
+        
+    }
+    return tmpStr;
+    
+}
